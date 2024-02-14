11a0135ee1e8e783e92057a9c848acc92ecb2c41
zippyh <zippyh@icloud.com>
2024-02-14 15:35:58 -0500

    Asks the user if they want to add or subtract

--- Calculator.java
+++ Calculator.java
@@ -5,10 +5,17 @@ public class Calculator {
         Scanner scan = new Scanner(System.in);
         int x;
         int y;
+        String operation;
         System.out.println("Enter two numbers:");
         x = scan.nextInt();
         y = scan.nextInt();
-        System.out.println(x + y);
+        System.out.println("Would you like to add of subtract the numbers?");
+        operation = scan.nextLine();
+        if(operation == "add") {
+            System.out.println(x + y);
+        } else if(operation == "subtract") {
+            System.out.println(x - y);
+        }
     }
     
 }
