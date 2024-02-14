f14cdadbf8619ee92b42c8a98c7c0fd2ca255709
zippyh <zippyh@icloud.com>
2024-02-14 15:29:30 -0500

    Asks the user for two numbers and adds them together

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args){
-        
+        Scanner scan = new Scanner(System.in);
+        int x;
+        int y;
+        System.out.println("Enter two numbers:");
+        x = scan.nextInt();
+        y = scan.nextInt();
+        System.out.println(x + y);
     }
     
 }
