8ce5ebcb6d3c5e806f23dddaafecb126b783b8a6
zippyh <zippyh@icloud.com>
2024-02-14 15:47:19 -0500

    Fixed errors

--- Calculator.java
+++ Calculator.java
@@ -3,21 +3,22 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        int x;
-        int y;
+        double x;
+        double y;
         String operation;
         System.out.println("Enter two numbers:");
-        x = scan.nextInt();
-        y = scan.nextInt();
+        x = scan.nextDouble();
+        y = scan.nextDouble();
+        operation = scan.nextLine();
         System.out.println("Would you like to add, subtract, multiply, or divide the numbers?");
         operation = scan.nextLine();
-        if(operation == "add") {
+        if(operation.equals("add")) {
             System.out.println(x + y);
-        } else if(operation == "subtract") {
+        } else if(operation.equals("subtract")) {
             System.out.println(x - y);
-        } else if(operation == "multiply") {
+        } else if(operation.equals("multiply")) {
             System.out.println(x * y);
-        } else if(operation == "divide") {
+        } else if(operation.equals("divide")) {
             System.out.println(x / y);
         }
     }
